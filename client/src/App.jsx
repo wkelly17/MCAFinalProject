6ef211abc73287eb223d0a8ce5ce44602db9e9e4
Will <willkelly9617@gmail.com>
2021-04-22 16:16:25 -0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen bg-red-500 min-h-screen">I am the app</div>
+  );
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'virtual:windi.css';
+// ! Dev tools;
+// import 'virtual:windi-devtools';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
